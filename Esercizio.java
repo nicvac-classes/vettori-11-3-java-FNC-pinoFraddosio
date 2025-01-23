76bd65bf2cc2a389c840bcb99a4f8a7343ac6138
FNC-pinoFraddosio <benedetto.fraddosio@marconibari.edu.it>
2025-01-23 19:08:48 +0000

    Consegna 1

--- Esercizio.java
+++ Esercizio.java
@@ -6,20 +6,71 @@ import java.util.Scanner;
 // Classe principale, con metodo main
 class Esercizio {
     // Il programma parte con una chiamata a main().
+
+    static void InserisciElemento(){
+
+    }
+    static void EliminaElemento(){
+
+    }
+    static void ricercaNelVettore(int V, int valore, int dimensione){
+        int i, j;
+        j = 0;
+        while(i = 0 && j < dimensione){
+            if(valore == V[j]){
+                i = j;
+            }
+        }
+    }
+    static void ElminaDuplicati(){
+
+    }
+    static void VisualizzaVettore(int V, int dimensione){
+        for(int i = 0; i < dimensione; i++){
+            System.out.println((i+1) + "° numero: " + V[i]);
+        }
+    }
+
     public static void main(String args[])
     {
-        //Variabili del programma
-        String nome;
 
-        //Creo l'oggetto in per l'input da tastiera
         Scanner in = new Scanner( System.in );
+        int N, o , valore;
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        System.out.print("Inserire grandezza vettore");
+        N = in.nextInt();
+        int[] V = new V[N];
+        
+        for(int i=0; i<0; i++){
+            V[i] = 0;    
+        }
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        do{
+            System.out.println("Scegli un'opzione: \n 1. Inserisci elemento \n 2. Elimina elemento \n 3. Ricerca elemento \n 4. Elimina duplicati \n 5. Visualizza vettore \n 6. Esci")
+            System.out.print("Input: ")
+            o = in.nextInt();
+            if(o == 0){
+                InserisciElemento();
+            }
+            if(o == 2){
+                EliminaElemento();
+            }
+            if(o == 3){
+                System.out.println("Inserire valore da ricercare: ");
+                valore = in.nextInt();
+                valore = ricercaNelVettore(V[N], valore, N);
+                System.out.println("Il valore è presente nella posizione: " + valore);
+            }
+            if(o == 4){
+                EliminaDuplicati();
+            }
+            if(o == 5){
+                VisualizzaVottre(V[N], N);
+            }
+            if(o != 1 || o != 2 || o != 3 || o != 4 || o != 5 || o != 6){
+                System.out.println("Inserire un valore valido");
+            }
+        }while(o != 6);
     }
 }
 
