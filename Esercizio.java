ba1650792e9d01e2262073c6d4cfde444133419f
FNC-pinoFraddosio <benedetto.fraddosio@marconibari.edu.it>
2025-02-04 19:04:10 +0000

    Consegna

--- Esercizio.java
+++ Esercizio.java
@@ -25,22 +25,31 @@ class Esercizio {
         nDimensione = dimensione + 1; 
         return nDimensione;
     }
-    static int EliminaElemento(int[] V, int dimensione, int i){
-        int N;
-        int[] W = new int[dimensione - 1];
+    static int EliminaElemento(int[] V, int N, int ie){
+        int N2;
+        int i;
+        N2 = N-1;
+        int[] W = new int[N2];
 
-        for(int j = 0; j <= i; j++){
-            W[j] = V[j];
-        } 
-        for(int j = i+1; j < dimensione - 1; j++){
-            W[j] = V[j+1];
-        }
-        for(int j = 0; j < dimensione - 1; j++){
-            V[j] = W[j];
+        i = 0;
+        while(i <= ie-1 && i < N){
+            W[i] = V[i];
+            i++;
         }
 
-        N = dimensione - 1;
-        return N;
+        i = ie+1;
+        while(i < N){
+            W[i-1] = V[i];
+            i++;
+        }
+
+        i = 0;
+        while(i < N2){
+            V[i] = W[i];
+            i++
+        }
+
+        return N2;
     }
     static int ricercaNelVettore(int[] V, int dimensione, int valore){
 
@@ -55,13 +64,39 @@ class Esercizio {
         }
         return i;
     }
+
     static int ElminaDuplicati(int[] V , int dimensione){
         return dimensione -1;
     }
+
     static void VisualizzaVettore(int[] V, int dimensione){
         for(int i = 0; i < dimensione; i++){
             System.out.println((i+1) + "° numero: " + V[i]);
         }
+    }
+
+    static int EliminaElementiOtt(int[] V, int N, int ie){
+        int N2;
+        int i;
+        N2 = N - 1;
+    
+        i = ie;
+        while(i <= N-2){
+            V[i] = V[i+1];
+            i = i+1;
+        }       
+        return N2;
+    }
+
+    static int InserisciElementoOtt(int[] V, int N, int e, int ie){
+        int i;
+        int N2;
+        N2 = N + 1;
+        for(i = N; i <= ie + 1; i--){
+            V[i] = V[i-1];
+        }
+        V[ie] = e;
+        return N2;
     }
 
     //MAIN
@@ -81,7 +116,7 @@ class Esercizio {
         }
 
         do{
-            System.out.println("Scegli un'opzione: \n 1. Inserisci elemento \n 2. Elimina elemento \n 3. Ricerca elemento \n 4. Elimina duplicati \n 5. Visualizza vettore \n 6. Esci");
+            System.out.println("Scegli un'opzione: \n 1. Inserisci elemento \n 2. Elimina elemento \n 3. Ricerca elemento \n 4. Elimina duplicati \n 5. Visualizza vettore \n 6. Elimina Elemento Ott. \n 7. Inserisci Elemento Ott. \n 8.Esci");
             System.out.print("Input: ");
             o = in.nextInt();
             if(o == 1){
@@ -117,10 +152,26 @@ class Esercizio {
             if(o == 5){
                 VisualizzaVettore(V,N);
             }
-            if(o != 1 && o != 2 && o != 3 && o != 4 && o != 5 && o != 6){
+            if(o == 6){
+                do{
+                    System.out.print("Inserire la posizione dell'elemento da eliminare: ");
+                    i = in.nextInt();
+                }while(i < 0 || i >= N);
+                N = EliminaElementiOtt(V, N, i);
+            }
+            if(o == 7){
+                System.out.print("Inserire il valore da aggiungere: ");
+                valore = in.nextInt();
+                do{
+                    System.out.print("Inserire la posizione in cui si deve inserire il valore: ");
+                    i = in.nextInt();
+                }while(i<0 || i>=N);
+                N = InserisciElementoOtt(V,N,valore,i);
+            }
+            if(o < 1 || i >9){
                 System.out.println("Inserire un valore valido");
             }
-        }while(o != 6);
+        }while(o != 9);
     }
 }
 
